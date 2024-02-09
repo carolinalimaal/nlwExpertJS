81c0309cf1fda684f4b9ed456c8d286c1e54c31f
carolinalimaal <carol.lima.alves2005@gmail.com>
2024-02-08 22:22:25 -0300

    verificação de acertos e contagem

--- script.js
+++ script.js
@@ -1,93 +1,138 @@
 const perguntas = [
     {
-        pergunta: "O que é JavaScript?",
+        pergunta: "1. Qual foi o primeiro console de videogame lançado no mercado?",
         respostas: [
-            "Uma linguagem de programação para estilização de páginas web.",
-            "Uma linguagem de programação para criação de layouts responsivos.",
-            "Uma linguagem de programação para desenvolvimento de páginas web interativas."
-        ],
-        correta: 2
-    },
-    {
-        pergunta: "Qual é a função do operador '===' em JavaScript?",
-        respostas: [
-            "Comparação de valores sem verificar o tipo.",
-            "Comparação de valores e tipos de dados.",
-            "Concatenação de strings."
+            "Atari 2600",
+            "Magnavox Odyssey",
+            "Nintendo Entertainment System (NES)"
         ],
         correta: 1
     },
     {
-        pergunta: "Qual é a forma correta de declarar uma variável em JavaScript?",
+        pergunta: "2. Em que ano foi lançado o icônico jogo 'Pong', considerado o primeiro jogo eletrônico de sucesso?",
         respostas: [
-            "variable x = 5;",
-            "let x = 5;",
-            "const x = 5;"
+            "1972",
+            "1980",
+            "1968"
         ],
-        correta: 2
+        correta: 0
     },
     {
-        pergunta: "Qual é o método utilizado para adicionar um elemento ao final de um array em JavaScript?",
+        pergunta: "3. Qual foi o primeiro console a apresentar gráficos 3D poligonais, revolucionando a indústria dos videogames?",
         respostas: [
-            "add()",
-            "push()",
-            "append()"
+            "Nintendo 64",
+            "PlayStation",
+            "Sega Genesis"
         ],
         correta: 1
     },
     {
-        pergunta: "Como se referencia um elemento HTML utilizando JavaScript?",
+        pergunta: "4. Qual empresa lançou o console Game Boy, um dos primeiros consoles de videogame portáteis?",
         respostas: [
-            "getElementById()",
-            "selectElement()",
-            "findElement()"
+            "Nintendo",
+            "Sony",
+            "Sega"
         ],
         correta: 0
     },
     {
-        pergunta: "Qual é a função do operador '&&' em JavaScript?",
+        pergunta: "5. Em que ano a Nintendo lançou o console Super Nintendo Entertainment System (SNES)?",
         respostas: [
-            "Operador de comparação.",
-            "Operador de atribuição.",
-            "Operador lógico 'E' (AND)."
+            "1989",
+            "1995",
+            "1990"
         ],
         correta: 2
     },
     {
-        pergunta: "O que é uma função de callback em JavaScript?",
+        pergunta: "6. Quem foi o criador do famoso personagem Mario, protagonista de 'Super Mario Bros.'?",
         respostas: [
-            "Uma função que retorna um valor booleano.",
-            "Uma função passada como argumento para outra função.",
-            "Uma função que faz chamadas recursivas."
+            "Hideo Kojima",
+            "Shigeru Miyamoto",
+            "Yuji Naka"
         ],
         correta: 1
     },
     {
-        pergunta: "Qual é o método utilizado para remover o último elemento de um array em JavaScript?",
+        pergunta: "7. Qual console popularizou o conceito de controles com sensores de movimento?",
         respostas: [
-            "pop()",
-            "remove()",
-            "deleteLast()"
-        ],
-        correta: 0
-    },
-    {
-        pergunta: "O que é o DOM em JavaScript?",
-        respostas: [
-            "Um estilo de codificação em JavaScript.",
-            "Uma técnica para criptografar dados em JavaScript.",
-            "Uma interface de programação para interagir com elementos HTML."
+            "PlayStation 2",
+            "Xbox 360",
+            "Nintendo Wii"
         ],
         correta: 2
     },
     {
-        pergunta: "Qual é a forma correta de escrever um comentário de linha única em JavaScript?",
+        pergunta: "8. Qual jogo é frequentemente considerado o pioneiro do gênero 'mundo aberto'?",
         respostas: [
-            "// Este é um comentário de linha única.",
-            "<!-- Este é um comentário de linha única. -->",
-            "/* Este é um comentário de linha única. */"
+            "Grand Theft Auto III",
+            "The Legend of Zelda",
+            "Super Mario 64"
         ],
         correta: 0
+    },
+    {
+        pergunta: "9. Em que ano a Sony lançou o console PlayStation 2, um dos consoles mais vendidos da história?",
+        respostas: [
+            "1998",
+            "2000",
+            "2005"
+        ],
+        correta: 1
+    },
+    {
+        pergunta: "10. Qual foi o primeiro jogo a utilizar discos ópticos como mídia de armazenamento?",
+        respostas: [
+            "Nintendo 64",
+            "PlayStation",
+            "Sega Saturn"
+        ],
+        correta: 2
+    },
+    {
+        pergunta: "11. Qual foi o primeiro videogame a ser comercialmente lançado?",
+        respostas: [
+            "Pong",
+            "Magnavox Odyssey",
+            "Space Invaders"
+        ],
+        correta: 1
+    },
+    {
+        pergunta: "12. Em que ano foi lançado o console Xbox, da Microsoft?",
+        respostas: [
+            "2001",
+            "1998",
+            "2005"
+        ],
+        correta: 0
+    },
+    {
+        pergunta: "13. Qual jogo é frequentemente considerado o precursor dos jogos de corrida em perspectiva 3D?",
+        respostas: [
+            "Mario Kart",
+            "Gran Turismo",
+            "Out Run"
+        ],
+        correta: 2
+    },
+    {
+        pergunta: "14. Qual foi o primeiro console a introduzir a função de multiplayer online?",
+        respostas: [
+            "Xbox Live",
+            "PlayStation Network",
+            "Nintendo Wi-Fi Connection"
+        ],
+        correta: 0
+    },
+    {
+        pergunta: "15. Quem é conhecido como o 'pai do PlayStation', sendo uma figura chave no desenvolvimento do console?",
+        respostas: [
+            "Shigeru Miyamoto",
+            "Hideo Kojima",
+            "Ken Kutaragi"
+        ],
+        correta: 2
     }
 ];
 
@@ -97,7 +142,10 @@ const quiz = document.querySelector('#quiz')
 // querySelector é uma função de pesquisa de seletores (tags, classes e ids)
 const template = document.querySelector('template')
 
+const corretas = new Set()
 
+const totalDePerguntas = perguntas.length
+const mostrarAcertos = document.querySelector('#acertos span')
 
 // loop
 for (let item of perguntas) {
@@ -106,18 +154,45 @@ for (let item of perguntas) {
     const quizItem = template.content.cloneNode(true)
 
     // Para colocar cada pergunta
-    quizItem.querySelector('h2').textContent = item.pergunta
+    quizItem.querySelector('h3').textContent = item.pergunta
 
     for (let resposta of item.respostas){
 
         //Para clonar o padrão para respostas
-        const subItem = quizItem.querySelector('dl dt').cloneNode(true)
+        const dt = quizItem.querySelector('dl dt').cloneNode(true)
 
         // Para mudar o texto da resposta
-        subItem.querySelector('span').textContent = resposta
+        dt.querySelector('label').textContent = resposta
+        
+        // Para mudar o for do label (Associar o FOR com o ID)
+        dt.querySelector('label').setAttribute('for', 'pergunta-' + perguntas.indexOf(item) + '-resposta-' + item.respostas.indexOf(resposta))
+
+        // Para mudar o id do input (Associar o FOR com o ID)
+        dt.querySelector('input').setAttribute('id', 'pergunta-' + perguntas.indexOf(item) + '-resposta-' + item.respostas.indexOf(resposta))
+
+        // Para mudar a referência (atributo name) de cada resposta
+        dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
+
+        // Para mudar o valor (atributo value) de cada resposta
+        dt.querySelector('input').value = item.respostas.indexOf(resposta)
+
+        // Para pegar o valor da resposta
+        dt.querySelector('input').onchange = (event) => {
+            // Valor da resposta: event.target.value
+
+            // Para verificar se a resposta está correta
+            const isRight = event.target.value == item.correta
+            corretas.delete(item) // Deleta o item antes de verificar se está correto
+            if (isRight) {
+                // Se estiver correto, entra nessa condição e adiciona o item
+                corretas.add(item)
+            }
+            // Para atualizar o número de acertos
+            mostrarAcertos.textContent = corretas.size + ' de ' + totalDePerguntas
+        }
 
         // Para adicionar a resposta na tela
-        quizItem.querySelector('dl').appendChild(subItem)
+        quizItem.querySelector('dl').appendChild(dt)
     }
 
     // Para remover o template "Resposta A"
